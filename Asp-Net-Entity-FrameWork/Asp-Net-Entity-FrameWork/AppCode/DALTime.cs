93aa49b75a06710c1cd9e4e417a3497566fff953
ronaldo jose da silva <ronaldorjsilva@hotmail.com>
2016-06-21 22:26:18 -0300

    ajustes

---
+++ Asp-Net-Entity-FrameWork/Asp-Net-Entity-FrameWork/AppCode/DALTime.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Asp_Net_Entity_FrameWork.AppCode
+{
+    public class DALTime
+    {
+        public IQueryable<TB_Times> Retrieve()
+        {
+            return null;
+        }
+        public void Add(TB_Times time)
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+        public void Delete(int id)
+        {
+
+        }
+    }
+}
---
+++ Asp-Net-Entity-FrameWork/Asp-Net-Entity-FrameWork/formulario.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Asp_Net_Entity_FrameWork
+{
+    public partial class formulario : System.Web.UI.Page
+    {
+
+        protected void frmView_ItemInserted(object sender, FormViewInsertedEventArgs e)
+        {
+            gvtimes.DataBind();
+        }
+
+        protected void gvtimes_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            frmView.ChangeMode(FormViewMode.Edit);
+        }
+
+        protected void frmView_ItemUpdated(object sender, FormViewUpdatedEventArgs e)
+        {
+            gvtimes.DataBind();
+        }
+
+       
+    }
+}
